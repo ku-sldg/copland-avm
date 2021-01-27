7ab49c2308f83d47526316d049dea260eab0f24e
Adam Petz <ampetz@ittc.ku.edu>
2021-01-27 02:28:00 -0600

    experiments

--- src/Policy.v
+++ src/Policy.v
@@ -71,11 +71,106 @@ Inductive disclosure_event: Ev -> Plc -> (*Plc ->*) Evidence -> Plc -> Prop :=
     events (annotated t []) q ev ->
     disclosure_event ev q' e b ->
     disclosure_event (req i loc p q t) requester e b
-| asp_dis: forall i p id args requester,
-    disclosure_event (umeas i p id args) requester (eval_asp (ASPC id args) p mt) requester.
+| asp_dis: forall i p id args requester init,
+    disclosure_event (umeas i p id args) requester (eval_asp (ASPC id args) p init) requester.
 
 
 Inductive events: AnnoTerm -> Plc -> Evidence -> Ev -> Prop :=.
+
+Require Import Trace Main Term.
+
+Require Import ConcreteEvidence.
+
+Fixpoint evshape (e:EvidenceC) :=
+  match e with
+  | mtc => mt
+  | uuc i _ e' => uu i [] 0 (evshape e')
+  | ggc p e' => gg p (evshape e')
+  | hhc p e' => hh p (evshape e')
+  | nnc i _ e' => nn i (evshape e')
+  | ssc e1 e2 => ss (evshape e1) (evshape e2)
+  | ppc e1 e2 => ss (evshape e1) (evshape e2)
+  end.
+
+
+Lemma evshape_eval: forall init,
+    Ev_Shape init (evshape init).
+Proof.
+Admitted.
+
+Inductive trace: AnnoTerm -> Plc -> Evidence ->
+                 list Ev -> Prop :=.
+
+Lemma cvm_respects_disclosed': forall t ev tr p bad_p requester e  et,
+  well_formed t ->
+  (* copland_compile t (mk_st init_ev [] p o) = (Some tt, (mk_st e' tr p' o')) -> *)
+  (*st_trace (run_cvm t
+                    (mk_st e [] p o)) = tr -> *)
+  (*st_trace
+    (run_cvm t (mk_st init_ev [] p o)) = tr -> *)
+
+   trace t p et tr ->
+   In ev tr ->                            
+  disclosure_event ev requester e bad_p ->
+  (*Ev_Shape init_ev et -> *)
+  evidence_disclosed requester p et (unanno t) e bad_p.
+Proof.
+  intros.
+  generalizeEverythingElse t.
+  induction t; intros.
+  -
+    destruct a.
+    (*
+      solve_by_inversion.
+  -
+    invc H1.
+    invc H3.
+    invc H4.
+    invc H5.
+    invc H3.
+    solve_by_inversion.
+  -
+    solve_by_inversion.
+  -
+    solve_by_inversion.
+  -
+    solve_by_inversion.
+Defined.
+     *)
+Admitted.
+
+(*
+    invc H0
+      try (
+    
+      invc H0;
+      cbn;
+      invc H1; solve_by_inversion).
+    +
+      (*
+      assert (Ev_Shape init_ev (evshape init_ev)).
+      {
+        eapply evshape_eval; eauto.
+      }
+      
+      assert (et = evshape init_ev). admit.
+      subst.
+       *)
+
+      (*
+      
+      
+      invc H4.
+      invc H1; try solve_by_inversion.
+      cbn.
+      invc H2.
+      assert (et = init). admit.
+      subst.
+      econstructor.
+  -
+*)
+Admitted.
+*)
 
 Lemma cvm_respects_disclosed: forall t ev tr p bad_p requester o e init_ev et e' p' o',
   well_formed t ->
@@ -90,13 +185,19 @@ Lemma cvm_respects_disclosed: forall t ev tr p bad_p requester o e init_ev et e'
   evidence_disclosed requester p et (unanno t) e bad_p.
 Proof.
   intros.
-  assert (events t p e ev).
-  {
-    
+    assert (trace t p et tr).
+    {
+      (*
+    eapply lstar_trace.
+    eapply wf_implies_wfr; eauto.
 
-
-  
+    eapply cvm_refines_lts_event_ordering; eauto.
+       *)
+      admit.
+    }
+    eapply cvm_respects_disclosed'; eauto.
 Admitted.
+
 
 
 
@@ -193,24 +294,7 @@ Lemma derive_passes_policy:
 Proof.
 Admitted.
 
-Require Import ConcreteEvidence.
 
-Fixpoint evshape (e:EvidenceC) :=
-  match e with
-  | mtc => mt
-  | uuc i _ e' => uu i [] 0 (evshape e')
-  | ggc p e' => gg p (evshape e')
-  | hhc p e' => hh p (evshape e')
-  | nnc i _ e' => nn i (evshape e')
-  | ssc e1 e2 => ss (evshape e1) (evshape e2)
-  | ppc e1 e2 => ss (evshape e1) (evshape e2)
-  end.
-
-
-Lemma evshape_eval: forall init,
-    Ev_Shape init (evshape init).
-Proof.
-Admitted.
 
 Lemma derive_passes_cvm_policy:
   forall secrets requester me init,
