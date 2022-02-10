75ddc280b3136648f02e25cff59597610129fe6b
Adam Petz <ampetz@ittc.ku.edu>
2022-02-10 17:22:11 -0600

    extracting some Flexible Mechanisms example phrases

---
+++ src/Example_Phrases.v
@@ -0,0 +1,138 @@
+Require Import Term_Defs Example_Phrases_Admits.
+
+Require Import List.
+Import ListNotations.
+
+Definition term1 := att 1 (asp SIG).
+                              
+
+Definition P0 : Plc := 0.
+Definition P1 : Plc := 1.
+Definition P2 : Plc := 2.
+Definition P3 : Plc := 3.
+Definition P4 : Plc := 4.
+
+Definition attest (p:Plc) (targ: TARG_ID) :=
+  asp (ASPC (asp_paramsC attest_id [] p targ)).
+
+Definition appraise (p:Plc) (targ: TARG_ID) :=
+  asp (ASPC (asp_paramsC appraise_id [] p targ)).
+
+Definition certificate (p:Plc) (targ: TARG_ID) :=
+  asp (ASPC (asp_paramsC cert_id [] p targ)).
+
+Definition store (p:Plc) (targ: TARG_ID) :=
+  asp (ASPC (asp_paramsC store_id [] p targ)).
+
+Definition retrieve (p:Plc) (targ: TARG_ID) :=
+  asp (ASPC (asp_paramsC retrieve_id [] p targ)).
+
+
+(* 
+pg 29:16 top, Certificate-Style section 
+ *)
+Definition cert_style_simple_sig : Term :=
+  att P1 (lseq
+            (attest P1 sys)
+            (att 2 (lseq
+                      (appraise P2 sys)
+                      (asp SIG)))).
+
+(* 
+pg 29:15, Certificate-Style section 
+ *)  
+Definition cert_style : Term :=
+  att P1 (lseq
+           (attest P1 sys)
+           (att 2 (lseq
+                    (appraise P2 sys)
+                    (certificate P2 sys)))).
+           
+
+(* 
+pg. 29:16 bottom, Certificate-Style section
+*)
+Definition cert_cache_p1 : Term :=
+  lseq
+    (attest P1 sys)
+    (lseq
+       (att P2
+            (lseq
+               (appraise P2 sys)
+               (certificate P2 sys)))
+       (store P1 cache)).
+
+Definition cert_cache_p0 : Term :=
+  att P1
+      (lseq
+         (bseq (NONE,ALL)
+               (retrieve P1 cache)
+               (asp CPY))
+         (asp SIG)).
+  
+(* 
+pg. 29:17, Background Check section 
+ *)
+Definition bg_check : Term :=
+  lseq
+    (att P1 (attest P1 sys))
+    (att P2 (appraise P2 sys)).
+
+
+(*
+pg. 29:18, Parallel Mutual Attestation section
+TODO:  Do subscripts of attest and appraise in text necessitate special ASP args?
+*)
+Definition par_mut_p0 : Term :=
+  lseq
+    (att P1 (attest P1 sys))
+    (att P2 (appraise P2 sys)).
+  
+Definition par_mut_p1 : Term :=
+  lseq
+    (att P0 (attest P0 sys))
+    (att P2 (appraise P2 sys)).
+
+
+(*
+pg. 29:19, Layered Background Check section
+ *)
+
+Definition layered_bg' : Term :=
+  lseq
+    (attest P1 sys)
+    (lseq
+       (attest P3 att_tid)
+       (attest P4 att_tid)).
+
+Definition layered_bg'' : Term :=
+  bpar (ALL,ALL)
+       (att P3 (attest P3 sys))
+       (att P4 (attest P4 sys)).
+
+Definition layered_bg_weak : Term :=
+  att P1
+      (lseq
+         (bpar (ALL,ALL)
+               layered_bg'
+               layered_bg'')
+         (att P2
+              (lseq
+                 (appraise P2 it)
+                 (asp SIG)))).
+         
+
+(*
+pg. 29:20, Layered Background Check section 
+ *)
+Definition layered_bg_strong : Term :=
+    att P1
+      (lseq
+         (bseq (ALL,ALL) (* only change from layered_bg_1 on this line (bpar --> bseq) *)
+               layered_bg'
+               layered_bg'')
+         (att P2
+              (lseq
+                 (appraise P2 it)
+                 (asp SIG)))).
+  
---
+++ src/Example_Phrases_Admits.v
@@ -0,0 +1,28 @@
+Require Import Term_Defs.
+
+Definition attest_id : ASP_ID.
+Admitted.
+
+Definition appraise_id : ASP_ID.
+Admitted.
+
+Definition cert_id : ASP_ID.
+Admitted.
+
+Definition store_id : ASP_ID.
+Admitted.
+
+Definition retrieve_id : ASP_ID.
+Admitted.
+
+Definition sys : TARG_ID.
+Admitted.
+
+Definition cache : TARG_ID.
+Admitted.
+
+Definition att_tid : TARG_ID.
+Admitted.
+
+Definition it : TARG_ID.
+Admitted.
