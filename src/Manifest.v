6c08b0e3df96a22cc675bcea309fe99ea7deca65
Adam Petz <ampetz@ittc.ku.edu>
2023-03-28 17:21:30 -0500

    more manifest musings..

--- src/Manifest.v
+++ src/Manifest.v
@@ -23,8 +23,7 @@ Definition PlcMap := MapC Plc Address.
 
 
 (** [Manifest] defines an attestation manger, a list of ASPs, and other
-   * managers it is aware of (a single AM and its
-   * interconnections).
+   * managers it is aware of (a single AM and its interconnections).
    *)
   Record Manifest := {
 
@@ -36,11 +35,8 @@ Definition PlcMap := MapC Plc Address.
     (* TO DO: Add privacy and selection policies to manifest? *)
                     }.
 
-
-
-
-
-  
+(** Representation of a system's environment/resources used to populate a 
+    ConcreteManifest based on an abstract Manifest. *)
   Record SystemConfig := {
     AM_Addresses : list AM_Address ;
     ASP_Addresses : list ASP_Address ;
@@ -51,13 +47,16 @@ Definition PlcMap := MapC Plc Address.
 
   
  (* A ConcreteManifest is a refinement of Manifest with concrete parameters
-    more suitable for extraction and deployment 
-  *)
+    more suitable for extraction and deployment.  *)
   Record ConcreteManifest := {
         ASP_Server : ASP_Address ;
         Pubkey_Server : ASP_Address;
         Plc_Map : MapC Plc AM_Address;
         Pubkey_Map : MapC Plc PublicKey
-                          
+                            }.
 
-    }.
+
+
+  (* This is a rough type signature for the "manifest compiler".  Still some details to be ironed out... *)
+  Definition manifest_compiler : Manifest -> SystemConfig -> ConcreteManifest.
+  Admitted.
