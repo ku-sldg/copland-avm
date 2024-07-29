d87c28ba57dbae593dfbf329a4b231fedca5f889
Will Thomas <30wthomas@gmail.com>
2024-07-29 10:22:48 -0500

    Adding a concrete extractables file for stuff we need in stubs

---
+++ src/Concrete_Extractables.v
@@ -0,0 +1,19 @@
+Require Import Manifest JSON Manifest_JSON Attestation_Session.
+Require Import List.
+Import ListNotations.
+
+(* This file contains things that we would like to be extracted
+for usage in stub code, but we do not have any good place to plug
+in outside of a list of extra stuff. *)
+
+Definition concrete_Jsonifiable_Manifest : Jsonifiable Manifest :=
+  Jsonifiable_Manifest.
+
+Definition concrete_Jsonifiable_ASP_Compat_MapT : Jsonifiable ASP_Compat_MapT.
+solve [typeclasses eauto].
+Defined.
+
+Definition concrete_Jsonifiable_Attestation_Session : Jsonifiable Attestation_Session.
+solve [typeclasses eauto].
+Defined.
+
--- src/Extraction_Cvm_Cake.v
+++ src/Extraction_Cvm_Cake.v
@@ -14,6 +14,8 @@ Require Import Manifest_Generator_Union Manifest_JSON Flexible_Mechanisms.
 Require Import List.
 Import ListNotations.
 
+Require Import Concrete_Extractables.
+
 Require Import ExtrCakeMLNativeString.
 
 Extraction Language CakeML. (* OCaml. *) 
@@ -26,14 +28,11 @@ Extract Constant hsh_params => "( undefined () )".
 (* Extract Constant + => "add". *)
 (* Extract Constant Nat.add => "(+)". *)
 
-(*
-Extract Constant get_ev => "bind get (fn st => ret (st_ev st)) : cvm_st -> coq_EvC".
-(* "st <- (@get cvm_st CVM_Error) ;; ret (st_ev st)". *)
-*)
-
 Separate Extraction 
     flexible_mechanisms flexible_mechanisms_evidence
-    run_cvm session_config_compiler Jsonifiable_Manifest
+    run_cvm session_config_compiler 
 		handle_AM_request end_to_end_mangen_final
-    run_demo_client_AM
+    run_demo_client_AM concrete_Jsonifiable_Manifest
+    concrete_Jsonifiable_ASP_Compat_MapT
+    concrete_Jsonifiable_Attestation_Session
     Jsonifiable_Evidence_Plc_list Jsonifiable_Term_Plc_list.
