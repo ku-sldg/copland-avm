b210536cbc35790e96e966932eae286a633ceaf4
Will Thomas <30wthomas@gmail.com>
2024-07-30 12:28:50 -0500

    Adding a README and test_all takes arg

--- src/tools/test_all_files.sh
+++ src/tools/test_all_files.sh
@@ -3,7 +3,7 @@ set -eu
 
 TOOLS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for f in *.v; do
+for f in $1/*.v; do
   echo "Processing $f"
   $TOOLS_DIR/find_all_deps.sh "$f" | $TOOLS_DIR/validate_all_deps.sh
 done
